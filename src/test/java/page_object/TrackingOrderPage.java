069e0a6c7d0a80743956cd7142c5aa87f9c3a53c
Artem Varnavskiy <avarnavskiy@gmail.com>
2023-05-21 12:17:33 +0300

    Проект с автотестами 4 спринт

---
+++ src/test/java/page_object/TrackingOrderPage.java
@@ -0,0 +1,18 @@
+package page_object;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+public class TrackingOrderPage {
+
+    private WebDriver driver;
+
+    private By orderNotFoundImage = By.xpath("//img[@alt='Not found']");
+
+    public TrackingOrderPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void checkOrderNotFound() {
+        driver.findElement(orderNotFoundImage);
+    }
+}
